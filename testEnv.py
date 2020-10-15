2e7f35993ea42fe5f55271d780ef08e207a635c8
sublime <sublime@vt.edu>
2020-10-15 16:04:54 -0400

    'make info' format improvements; more tests

--- testEnv.py
+++ testEnv.py
@@ -1,48 +1,50 @@
-import os
-import sys
-import shutil
-from pprint import pprint
 ''' A quick test of how well ODSA will handle in the current environment.
 All output lines should output 'True' for a good environment.
 '''
 
-# print("PATH=")
-# pprint(os.getenv("PATH").split(os.pathsep))
+import os
+import sys
+import shutil
+from importlib.util import find_spec
 
+def testInPath(s):
+	pathStr = '\n'.join(sys.path)
+	print(s in pathStr, ":", s, "in sys.path")
+
+def testIsCommandFound(s):
+	cmdFound = shutil.which(s) is not None
+	print(cmdFound, ":", s, "command visible from python")	
+
+# print("PATH=", *os.getenv("PATH").split(os.pathsep), sep='\n   ')
+# print("sys.path=", *sys.path, sep='\n   ')
+
+print("### Requirements for OpenDSA: ")
 print('.pyVenv' in sys.prefix, ": .pyVenv in sys.prefix")
 print('.pyVenv' in sys.exec_prefix, ": .pyVenv in sys.exec_prefix")
 print('.pyVenv' not in sys.base_prefix, ": .pyVenv NOT in sys.base_prefix")
 
-joinedPath = ';'.join(sys.path)
+testInPath("hieroglyph" + os.sep + "src")
+testInPath(".pyVenv")
+testInPath(".pyVenv" + os.sep + "lib" + os.sep + "site-packages")
 
-def isInPath(s):
-	print(s in joinedPath, ":", s, "in sys.path")
-isInPath("hieroglyph" + os.sep + "src")
-isInPath(".pyVenv")
-isInPath(".pyVenv" + os.sep + "lib" + os.sep + "site-packages")
-
-# print("sys.path=")
-# pprint(sys.path)
-
-hasSphinx = False
-try:
-	import sphinx
-	hasSphinx = 'sphinx' in sys.modules
-except:
-	pass
-
+hasSphinx = find_spec("sphinx") is not None
 print(hasSphinx, ": sphinx in sys.modules")
 
-def isCommandFound(s):
-	cmdFound = shutil.which(s) is not None
-	print(cmdFound, ":", s, "command visible from python")	
-isCommandFound('make')
-isCommandFound('sphinx-build')
+testIsCommandFound('make')
+testIsCommandFound('python')
+testIsCommandFound('sphinx-build')
 
-
-pythonFound = shutil.which('python') is not None
-print(pythonFound, ": python can run another python")
 pyVenvFound = '.pyVenv' in str(shutil.which('python'))
-print(pyVenvFound, ": python can run another VENV python")
+print(pyVenvFound, ": python command keeps the VENV python")
+
+print("### Minify tools needed for production environment:")
+testIsCommandFound('uglifyjs')
+testIsCommandFound('cleancss')
+
+print("### Optional Linting tools:")
+testIsCommandFound('pylint')
+testIsCommandFound('jsonlint')
+testIsCommandFound('eslint')
+testIsCommandFound('csslint')
 
 print()
